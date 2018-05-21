6f6ec90079c91d04f11a563fea6fdac91fd83f8c
wufenyun <wufenyun@zbj.com>
2018-05-21 12:21:16 +0800

    update some demo

---
+++ src/main/java/com/learning/concurrent/SimpleBlockQueue.java
@@ -0,0 +1,15 @@
+/**
+ * Package: com.app.concurrent
+ * Description: 
+ */
+package com.learning.concurrent;
+
+/**
+ * Description:  
+ * Date: 2017年8月4日 下午4:42:06
+ * @author wufenyun 
+ */
+public interface SimpleBlockQueue<E> {
+    
+    void add(E e);
+}
